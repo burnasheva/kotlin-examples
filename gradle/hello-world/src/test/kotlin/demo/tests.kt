ef19c35d9d696b6bb121d5eeb0d1bf63b9f8e6de
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-08-27 10:24:41 +0200

    update dependencies and add more tests

--- gradle/hello-world/src/test/kotlin/demo/tests.kt
+++ gradle/hello-world/src/test/kotlin/demo/tests.kt
@@ -2,9 +2,23 @@ package demo
 
 import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class TestSource {
     @Test fun f() {
         assertEquals("Hello, world!", getGreeting())
+    }
+
+    @Test fun `first test should fail` () {
+        assertTrue ( false, "Something went wrong!" )
+    }
+
+    @Test fun `second test should fail` () {
+        assertFalse (true, "Something went wrong again!")
+    }
+
+    @Test fun `this test should pass` () {
+        assertFalse (false, "Something went wrong again!")
     }
 }

a9c4619a0b1cd55b0eb2c7728b7fd2cb85627f26
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-08-27 09:56:57 +0200

    update dependencies

--- maven/hello-world/src/test/kotlin/HelloTest.kt
+++ maven/hello-world/src/test/kotlin/HelloTest.kt
@@ -3,9 +3,23 @@ package hello.tests
 import hello.getHelloString
 import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class HelloTest {
     @Test fun testAssert() : Unit {
         assertEquals("Hello, world!", getHelloString())
+    }
+
+    @Test fun `validate() function is recursively called on all children` () {
+        assertTrue ( false, "Something went wrong!" )
+    }
+
+    @Test fun `validate() function throws an error when everything is bad` () {
+        assertFalse (true, "Something went wrong again!")
+    }
+
+    @Test fun `validate() this test should pass` () {
+        assertFalse (false, "Something went wrong again!")
     }
 }
